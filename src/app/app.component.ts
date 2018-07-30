7b07a72631ffeb3fa4e4587f723b53a8bfcb518b
Deep Parmar <deep56parmar@hotmail.com>
2018-07-30 20:54:30 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+
+export interface UserData {
+  cap24hrChange: number;
+  long: string;
+  mktcap: number;
+  perc: number;
+  price: number;
+  shapeshift: number;
+  short: number;
+  supply: number;
+  usdVolume: number;
+  volume: number;
+  vwapData: number;
+  vwapDataBTC: number;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CoinCap';
+
+
+
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['long', 'mktcap', 'price', 'vwapData', 'supply', 'volume', 'perc', 'shapeshift'];
+  dataSource: MatTableDataSource<UserData>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(private socket: Socket, private http: HttpClient) { }
+  ngOnInit() {
+    this.http.get('http://coincap.io/front').subscribe((res) => {
+      console.log(res);
+      this.dataSource = new MatTableDataSource(Object.values(res));
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      console.log(this.dataSource);
+    });
+    this.socket.on('trades', (update) => {
+      // console.log(update['msg']);
+      this.updates(update['msg']['long'], update['msg']);
+    });
+
+  }
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  updates(name, data) {
+    if (name) {
+      for (let i = 0; i < Object.keys(this.dataSource['data']).length; i++) {
+        if (name === this.dataSource.data[i]['long']) {
+          this.dataSource['data'][i]['cap24hrChange'] = data.cap24hrChange;
+          this.dataSource['data'][i]['long'] = data.long;
+          this.dataSource['data'][i]['mktcap'] = data.mktcap;
+          this.dataSource['data'][i]['perc'] = data.perc;
+          this.dataSource['data'][i]['price'] = data.price;
+          this.dataSource['data'][i]['shapeshift'] = data.shapeshift;
+          this.dataSource['data'][i]['short'] = data.short;
+          this.dataSource['data'][i]['supply'] = data.supply;
+          this.dataSource['data'][i]['usdVolume'] = data.usdVolume;
+          this.dataSource['data'][i]['volume'] = data.volume;
+          this.dataSource['data'][i]['vwapData'] = data.vwapData;
+          this.dataSource['data'][i]['vwapDataBTC'] = data.vwapDataBTC;
+          console.log('Update in' , name );
+        }
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// tslint:disable-next-line:max-line-length
+import { MatToolbarModule, MatButtonModule, MatCardModule, MatTableModule, MatPaginatorModule, MatFormFieldModule, MatInputModule, MatSortModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { HttpClientModule } from '@angular/common/http';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+const config: SocketIoConfig = { url: 'https://coincap.io', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    HttpClientModule,
+    MatToolbarModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
